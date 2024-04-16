02575b7ee04b092cd8109ff3bfcc92856ac5aae3
yankang <1944851588@qq.com>
2024-04-16 10:58:10 +0800

    优化

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react'
 import Count from './containers/count'   // 容器组件
 import Person from './containers/Person'   // 容器组件
 
+// import SetState from './containers/SetState'
+import UseStateHooks from './containers/Hooks/useState'
+
 export default class App extends Component {
   render() {
     return (
       <div>
         {/*count 容器组件不能自身引入 store，  需要父组件引入，通过 props 传递 */}
-        <Count />
+        {/* <Count />
         <hr />
-        <Person />
+        <Person /> */}
+
+        {/* setSate */}
+        {/* <SetState /> */}
+
+        <UseStateHooks />
       </div>
     )
   }
--- src/containers/count/index.jsx
+++ src/containers/count/index.jsx
@@ -1,9 +1,54 @@
 // 引入 UI 组件
-import CountUI from '../../component/Count'
+// import CountUI from '../../component/Count'
+import React, { Component } from 'react'
 // 引入 react-redux
 import { connect } from 'react-redux'
 import { increment, decrement, incrementAsync } from '../../redux/actions/count_action'
 
+
+class CountUI extends Component {
+
+  increment = () => {
+    const {value} = this.selectNumber
+    this.props.increment(value * 1)
+  }
+
+  decrement = () => {
+    const {value} = this.selectNumber
+    this.props.decrement(value * 1)
+  }
+
+  incrementIfOdd = () => {
+    const {value} = this.selectNumber
+    if(this.props.count % 2 !== 0 ) {
+      this.props.increment(value * 1)
+    }
+  }
+
+  incrementAsync = () => {
+    const {value} = this.selectNumber
+    this.props.incrementAsync(value * 1, 500)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>我是 count 组件</h2>
+        <h1>当前求和为:{this.props.count}</h1>
+        <h1>下方人数为：{this.props.personCount}</h1>
+        <select ref={c =>this.selectNumber = c}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>&nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;
+        <button onClick={this.decrement}>-</button>&nbsp;
+        <button onClick={this.incrementIfOdd}>基数求和</button>&nbsp;
+        <button onClick={this.incrementAsync}>异步求和</button>&nbsp;
+      </div>
+    )
+  }
+}
 
           // 初始版
 // 当只有一个 redux 暴露的时候 此时的 state == count  ，当暴露多个 redux 是，此时 state 是一个对象，需要 通过 key 取值
@@ -20,6 +65,8 @@ import { increment, decrement, incrementAsync } from '../../redux/actions/count_
 // }
 
 // export default connect(mapStateToProps, mapDispatchToProps)(CountUI)
+
+
 
 
           // 升级版
